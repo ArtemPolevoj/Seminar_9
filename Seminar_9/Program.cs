bb01ad65d6274fd1a9df964e193701a55747e33f
Артём <artempolevoj@yandex.ru>
2023-01-29 11:56:27 +0300

    Добавьте файлы проекта.

---
+++ Seminar_9/Program.cs
@@ -0,0 +1,22 @@
+﻿using Seminar_9;
+
+Console.WriteLine("Задача 64: Задайте значение N.Напишите программу, которая выведет\n" +
+ "все натуральные числа в промежутке от N до 1.");
+int number = Value.getInt("Введите целое число");
+Value.ValueOfN(number);
+Console.WriteLine();
+
+Console.WriteLine("Задача 66: Задайте значения M и N. Напишите программу,\n" +
+    " которая найдёт сумму натуральных элементов в промежутке от M до N.");
+int m = Value.getInt("Введите целое число");
+int n = Value.getInt("Введите целое число");
+int sum = 0;
+Console.WriteLine($"Сумма натуральных элементов" +
+    $" в промежутке от M до N = {Value.SumFromMtoN(m,n, ref sum)}");
+Console.WriteLine();
+
+Console.WriteLine("Задача 68: Напишите программу вычисления функции Аккермана\n" +
+    "с помощью рекурсии. Даны два неотрицательных числа m и n.");
+int nK = Value.getInt("Введите целое число");
+int mK = Value.getInt("Введите целое число");
+Console.WriteLine($"A(m,n) = {Value.Akkermana(nK, mK)}");
---
+++ Seminar_9/Value.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar_9
+{
+    internal class Value
+    {
+        public static int ValueOfN(int n)
+        {
+            Console.Write($"{n}, ");
+            if (n == 1) return 1;
+            else return n - ValueOfN(n - 1);
+        }
+
+        public static int SumFromMtoN(int m, int n, ref int sum)
+        {
+            // не забыть int sum = 0;
+            sum += m;
+            if (m == 0) return ((n * (n + 1)) / 2) + 1;
+            else if (n == 0) return ((m * (m + 1)) / 2) + 1;
+            else if (m == n) return 0;
+            else if (m == n) SumFromMtoN(m - 1, n, ref sum);
+            else SumFromMtoN(m + 1, n, ref sum);
+            return sum;
+        }
+
+        public static int Akkermana(int n, int m)
+        {
+            if (n == 0)
+                return m + 1;
+            else
+                if (m == 0)
+                return Akkermana(n - 1, 1);
+            else
+                return Akkermana(n - 1, Akkermana(n, m - 1));
+        }
+
+        public static int getInt(string str)
+        {
+            int number = 0;
+        checkNumber:
+            Console.WriteLine(str);
+            string? strNumber = Console.ReadLine();
+            try
+            {
+                if (strNumber != null) number = int.Parse(strNumber);
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine("Ввели не целое число или не верный формат числа.");
+                goto checkNumber;
+            }
+            catch (OverflowException ofe)
+            {
+                Console.WriteLine("Ввели большое число.");
+                goto checkNumber;
+            }
+
+            return number;
+        }
+    }
+}
